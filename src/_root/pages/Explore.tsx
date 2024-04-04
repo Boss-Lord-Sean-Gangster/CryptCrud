377c08846ee26cb6f17a761bd27601c46fa93818
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-04-04 16:26:00 +0530

    Update Explore.tsx

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -47,7 +47,7 @@ const Explore = () => {
 
   const shouldShowSearchResults = searchValue !== "";
   const shouldShowPosts = !shouldShowSearchResults && 
-    posts.pages.every((item) => item.documents.length === 0);
+    posts.pages.every((item) => item?.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -97,7 +97,7 @@ const Explore = () => {
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+           item&& <GridPostList key={`page-${index}`} posts={item.documents} />
           ))
         )}
       </div>
