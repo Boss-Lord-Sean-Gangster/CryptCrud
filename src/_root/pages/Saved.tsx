92272fbd90ffcfe03775b78d80a8bdbe0c5616ac
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-04-04 18:32:36 +0530

    Update Saved.tsx

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -2,7 +2,9 @@
 
 const Saved = () => {
   return (
-    <div>Saved</div>
+    <div className='flex align-center justify-center w-full h-full '>
+      <h2 >COMING SOON....</h2>
+    </div>
   )
 }
 
