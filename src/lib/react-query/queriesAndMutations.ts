c2b407dd4979a4689c3fb9b619c098aaeb45064b
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-04-04 18:07:03 +0530

    Update queriesAndMutations.ts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -73,22 +73,24 @@ export const useCreateUserAccount = () => {
     })
   }
 
-  export const useSavePost=()=>{
+export const useSavePost = () => {
     const queryClient = useQueryClient();
-
     return useMutation({
-      mutationFn:({postId , userId}: {postId: string , userId: string})=> 
-      savePost(postId , userId),
-      onSuccess:()=>{
-          queryClient.invalidateQueries({
-            queryKey:[QUERY_KEYS.GET_RECENT_POSTS]});
-            queryClient.invalidateQueries({
-              queryKey:[QUERY_KEYS.GET_POSTS]});
-              queryClient.invalidateQueries({
-                queryKey:[QUERY_KEYS.GET_CURRENT_USER]});
-      }
-    })
-  }
+      mutationFn: ({ userId, postId }: { userId: string; postId: string }) =>
+        savePost(userId, postId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
 
   export const useDeleteSavedPost=()=>{
     const queryClient = useQueryClient();
