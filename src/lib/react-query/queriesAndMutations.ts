e0bc587fa67ff9b477c6cc674aa1cb6feab0db99
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-04-04 17:46:29 +0530

    Update queriesAndMutations.ts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -149,19 +149,23 @@ export const useCreateUserAccount = () => {
     })
   }
 
-  export const useGetPosts=()=>{
+  export const useGetPosts = () => {
     return useInfiniteQuery({
-      queryKey:[QUERY_KEYS.GET_INFINITE_POSTS],
-      queryFn:getInfinitePosts,
-      getNextPageParam:(lastPage) => {
-        if(!lastPage || lastPage.documents?.length===0) return null;
-      const lastId = lastPage.documents[lastPage?.documents.length-1].$id;
-      return lastId
-    }
-    // HELLO
-  }
-)}
-
+      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+      queryFn: getInfinitePosts as any,
+      getNextPageParam: (lastPage: any) => {
+        // If there's no data, there are no more pages.
+        if (lastPage && lastPage.documents.length === 0) {
+          return null;
+        }
+  
+        // Use the $id of the last document as the cursor.
+        const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+        return lastId;
+      },
+      initialPageParam: null 
+    });
+  };
   export const useSearchPosts = (searchTerm:string)=>{
   return useQuery({
   queryKey:[QUERY_KEYS.SEARCH_POSTS, searchTerm],
